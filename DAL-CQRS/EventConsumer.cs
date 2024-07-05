a7d4ad14019183bcf45c4403620b0180a0d9a196
İkram MERT <ikram.mert@hotmail.com>
2024-07-05 17:56:08 +0300

    added event sourcing and consumer

---
+++ DAL-CQRS/EventConsumer.cs
@@ -0,0 +1,72 @@
+﻿using DAL_CQRS.Entities;
+using DAL_CQRS.Entities.Events;
+using MongoDB.Driver;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DAL_CQRS.Entities.Mongo;
+using DAL_CQRS.Repositories;
+
+namespace DAL_CQRS.EventConsumers
+{
+    public class EventConsumerService : BackgroundService
+    {
+        private readonly EventStore _eventStore;
+        private readonly QueryDbContext _queryDbContext;
+
+        public EventConsumerService(EventStore eventStore, QueryDbContext queryDbContext)
+        {
+            _eventStore = eventStore;
+            _queryDbContext = queryDbContext;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var filter = Builders<HotelEvent>.Filter.Eq(e => e.Processed, false);
+                var unprocessedEvents = await _eventStore.Events.Find(filter).ToListAsync();
+
+                foreach (var hotelEvent in unprocessedEvents)
+                {
+                    await HandleEvent(hotelEvent);
+                    hotelEvent.Processed = true;
+                    await _eventStore.Events.ReplaceOneAsync(e => e.Id == hotelEvent.Id, hotelEvent);
+                }
+
+                await Task.Delay(1000, stoppingToken); // 1 saniye bekle, sonra tekrar kontrol et
+            }
+        }
+
+        private async Task HandleEvent(HotelEvent hotelEvent)
+        {
+            switch (hotelEvent.EventType)
+            {
+                case "CreateHotel":
+                    var otelForMongo = new OtelMongo
+                    {
+                        OtelId = hotelEvent.OtelModel.OtelId.ToString(),
+                        OtelAdress = hotelEvent.OtelModel.OtelAdress,
+                        CreationTime = hotelEvent.OtelModel.CreationTime,
+                        IsActive = hotelEvent.OtelModel.IsActive,
+                        LastModificationTime = hotelEvent.OtelModel.LastModificationTime,
+                        Name = hotelEvent.OtelModel.Name,
+                        OtelWebSite = hotelEvent.OtelModel.OtelWebSite,
+                        Price = hotelEvent.OtelModel.Price
+                    }; 
+                    await _queryDbContext.Otels.InsertOneAsync(otelForMongo);
+                    break;
+
+                case "DeleteHotel":
+                    var otelId = hotelEvent.OtelModel.OtelId;
+                    await _queryDbContext.Otels.DeleteOneAsync(o=>o.OtelId == otelId.ToString());
+                    break;
+
+                default:
+                    // Bilinmeyen bir event tipi varsa burada işlem yapabilirsiniz.
+                    break;
+            }
+        }
+    }
+}
--- DAL-CQRS/Handlers/CommandHandlers/CreateOtelCommandHandler.cs
+++ DAL-CQRS/Handlers/CommandHandlers/CreateOtelCommandHandler.cs
@@ -1,10 +1,12 @@
 ﻿using DAL_CQRS.Commands.Request;
 using DAL_CQRS.Commands.Response;
 using DAL_CQRS.Entities;
+using DAL_CQRS.Entities.Events;
 using DAL_CQRS.Repositories;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,13 +16,14 @@ namespace DAL_CQRS.Handlers.CommandHandlers
     public class CreateOtelCommandHandler : IRequestHandler<CreateOtelCommandRequest, CreateOtelCommandResponse>
     {
         private readonly CommandDbContext _commandDbContext;
+        private readonly EventStore _eventStore;
         
-        public CreateOtelCommandHandler(CommandDbContext commandDbContext) {
+        public CreateOtelCommandHandler(CommandDbContext commandDbContext, EventStore eventStore) {
             _commandDbContext = commandDbContext;
+            _eventStore = eventStore;
         }
         public async Task<CreateOtelCommandResponse> Handle(CreateOtelCommandRequest request, CancellationToken cancellationToken)
         {
-            //var data = _commandDbContext.Add(request);
             var data = new Otel
             {
                 OtelId = request.OtelId,
@@ -30,6 +33,11 @@ namespace DAL_CQRS.Handlers.CommandHandlers
                 OtelWebSite = request.OtelWebSite,
                 Price = request.Price,
             };
+            var otelEvent = new HotelEvent("CreateHotel")
+            {
+                OtelModel = data
+            };
+            await _eventStore.SaveEventAsync(otelEvent);
             _commandDbContext.iK_Otels.Add(data);
             
             if (data == null)
@@ -41,20 +49,6 @@ namespace DAL_CQRS.Handlers.CommandHandlers
             }
             _ = await _commandDbContext.SaveChangesAsync(cancellationToken);
             return new CreateOtelCommandResponse() { IsSuccess = true, OtelId = data.OtelId};
-
-            //var id = Guid.NewGuid();
-            //ApplicationDbContext.OtelList.Add(new(){
-            //    OtelId = id,
-            //    Name = request.Name,
-            //    OtelAdress = request.OtelAdress,
-            //    IsActive = request.IsActive,
-            //    OtelWebSite = request.OtelWebSite
-            //});
-            //return new CreateOtelCommandResponse
-            //{
-            //    IsSuccess = true,
-            //    OtelId = id
-            //};
         }
     }
 }
